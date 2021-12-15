2b8779696fa9ed3a84ea088e3477fd6e015452d2
jdefez <jdefez@gmail.com>
2021-12-15 22:05:38 +0100

    removing galaxy-line config file

--- lua/which-key-bindings.lua
+++ lua/which-key-bindings.lua
@@ -5,23 +5,38 @@ wk.register({
   --  - phpactor (augroup?)
   --  - illuminate
   --  - gitsigns
-  --  - BDelete
+  b = {
+    name = '+buffer',
+    c = {'<cmd>BDelete this', 'Close current buffer'},
+    h = {'<cmd>BDelete hidden', 'Close hidden buffers'},
+    n = {'<cmd>BDelete nameless', 'Close nameless buffers'},
+  },
+  d = {
+    name = '+trouble',
+    t = {'<cmd>TroubleToggle<cr>', 'Toggle trouble'},
+    w = {'<cmd>TroubleToggle workspace_diagnostics<cr>', 'Toggle trouble diagnostics'},
+    d = {'<cmd>TroubleToggle document_diagnostics<cr>', 'Toggle document diagnostics'},
+    q = {'<cmd>TroubleToggle quickfix<cr>', 'Toggle quickfix'},
+    l = {'<cmd>TroubleToggle loclist<cr>', 'Toggle loclist'},
+    R = {'<cmd>TroubleToggle lsp_references<cr>', 'Toggle lsp references'}
+  },
   f = {
     name = '+telescope',
     f = {'<cmd>Telescope find_files<cr>', 'Find file'},
     g = {'<cmd>Telescope live_grep<cr>', 'Live grep'},
     b = {'<cmd>Telescope buffers<cr>', 'Buffers'},
     h = {'<cmd>Telescope help_tags<cr>', 'Tags'},
-    c = {'<cmd>Telescope git_commits<cr>', 'Git commits'},
-    s = {'<cmd>Telescope git_status<cr>', 'Git status'}
+    c = {'<cmd>Telescope git_commits<cr>', 'List commits'},
+    s = {'<cmd>Telescope git_status<cr>', 'Show git status'}
   },
-  x = {
-    name = '+trouble',
-    x = {'<cmd>TroubleToggle<cr>', 'Toggle trouble'},
-    w = {'<cmd>TroubleToggle workspace_diagnostics<cr>', 'Toggle trouble diagnostics'},
-    d = {'<cmd>TroubleToggle document_diagnostics<cr>', 'Toggle document diagnostics'},
-    q = {'<cmd>TroubleToggle quickfix<cr>', 'Toggle quickfix'},
-    l = {'<cmd>TroubleToggle loclist<cr>', 'Toggle loclist'},
-    R = {'<cmd>TroubleToggle lsp_references<cr>', 'Toggle lsp references'}
+  h = {
+    name = '+hunk',
+    s = {'<cmd>Gitsigns stage_hunk<CR>', 'Stage hunk'},
+    u = {'<cmd>Gitsigns undo_stage_hunk<CR>', 'Undo stage hunk'},
+    U = {'<cmd>Gitsigns reset_buffer_index<CR>', 'Reset buffer index'},
+    r = {'<cmd>Gitsigns reset_hunk<CR>', 'Reset hunk'},
+    R = {'<cmd>Gitsigns reset_buffer<CR>', 'Reset buffer'},
+    S = {'<cmd>Gitsigns stage_buffer<CR>', 'Stage buffer'},
+    p = {'<cmd>Gitsigns preview_hunk<CR>', 'Preview hunk'}
   }
 }, {prefix = '<leader>'})
