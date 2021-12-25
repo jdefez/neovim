fc4009a60edb82fab94ea7448c8f8b38a769ab64
jdefez <jdefez@gmail.com>
2021-12-25 10:44:25 +0100

    creating a seperate configuration file for lsp features

--- init.vim
+++ init.vim
@@ -1,3 +1,5 @@
+lua require('basic')
+
 lua require('plugins')
 
 augroup packer_user_config
@@ -5,24 +7,7 @@ augroup packer_user_config
   autocmd BufWritePost plugins.lua source <afile> | PackerCompile
 augroup end
 
-lua require('basic')
-
-"lua require'navigator'.setup()
-
-" lsp servers
-lua <<EOF
-require'lspconfig'.intelephense.setup{}
-require'lspconfig'.dockerls.setup{}
-require'lspconfig'.jsonls.setup{}
-require'lspconfig'.eslint.setup{}
-require'lspconfig'.yamlls.setup{}
-require'lspconfig'.dotls.setup{}
--- phpactor must be installed glabaly as described here:
---  https://phpactor.readthedocs.io/en/master/usage/standalone.html#installation-global
---  and the path to the executable must be set with
---  vim.g['phpactor_executable'] = '~/phpactor/bin/phpactor'
-require'lspconfig'.phpactor.setup{}
-EOF
+lua require('lsp-servers')
 
 lua require('nvim-cmp')
 
@@ -59,6 +44,7 @@ au BufRead,BufNewFile *.blade.php set ft=html
 autocmd Filetype vue setlocal expandtab tabstop=2 softtabstop=2 shiftwidth=2
 autocmd Filetype html setlocal expandtab tabstop=2 softtabstop=2 shiftwidth=2
 autocmd Filetype graphql setlocal expandtab tabstop=2 softtabstop=2 shiftwidth=2
+
 autocmd Filetype python setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
 autocmd Filetype apache setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
 autocmd Filetype php setlocal expandtab tabstop=4 softtabstop=4 shiftwidth=4
--- lua/nvim-cmp.lua
+++ lua/nvim-cmp.lua
@@ -1,48 +1,9 @@
--- function: on_attach attaching completion to lsp server
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
+
+-- plugin: nvim-cmp.
 
 local t = function(str)
   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
-
-
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  -- Enable completion triggered by <c-x><c-o>
-
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-
-  local opts = { noremap = true, silent = true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-  require 'illuminate'.on_attach(client)
-end
-
--- plugin: nvim-cmp.
 
 local cmp = require'cmp'
 
@@ -167,14 +128,6 @@ cmp.setup({
   })
 })
 
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-
--- cmp.setup.cmdline('/', {
---   sources = {
---     { name = 'buffer' }
---   }
--- })
-
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 
 cmp.setup.cmdline(':', {
@@ -184,17 +137,3 @@ cmp.setup.cmdline(':', {
     { name = 'cmdline' }
   })
 })
-
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local lspconfig = require('lspconfig')
-
-local servers = {'intelephense', 'dockerls', 'eslint', 'jsonls', 'dotls'}
-
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    flags = { debounce_text_changes = 150 },
-    capabilities = capabilities,
-  }
-end
